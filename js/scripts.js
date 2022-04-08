79b99f0d0e28519cede493f7e34d4a6b21969579
Olvar Abadeen <oabadeen@gmail.com>
2022-04-08 11:03:58 -0700

    Added Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-alert('Hello World');
- let favoriteFood = 'pizza';
- document.write(favoriteFood);
- 
+let pokemonList = [{
+  name: "Bulbasaur",
+  height: '0.7m',
+  type: 'grass,poison'
+}, {
+  name: "Squirtle",
+  height: '0.5m',
+  type: 'water'
+}, {
+  name: "Charmander",
+  height: '0.6m',
+  type: 'fire'
+}];
