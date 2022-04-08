ab0aed51861cd380f6cb3232b09fb30d9c58ad99
Olvar Abadeen <oabadeen@gmail.com>
2022-04-08 14:19:12 -0700

    added a loop to list pokemon with their height

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 let pokemonList = [{
   name: "Bulbasaur",
-  height: '0.7m',
+  height: '2\'04\"',
   type: 'grass,poison'
 }, {
   name: "Squirtle",
-  height: '0.5m',
+  height: '1\'08\"',
   type: 'water'
 }, {
   name: "Charmander",
-  height: '0.6m',
+  height: '2\'',
   type: 'fire'
 }];
+
+//loop that list all pokemon with the height and name
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write("Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + ".");
+}
+//conditional if a pokemon has a height above 2 it is big
+if (pokemonList.height < 2);
+document.write(pokemonList[i].height + 'Wow that is big');
