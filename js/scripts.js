2a1b136f0aff85fb86436e7a2df804cf8844007a
Olvar Abadeen <oabadeen@gmail.com>
2022-04-08 18:41:23 -0700

    fixed height value and height conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-let pokemonList = [{
-  name: "Bulbasaur",
-  height: '2\'04\"',
-  type: 'grass,poison'
-}, {
-  name: "Squirtle",
-  height: '1\'08\"',
-  type: 'water'
-}, {
-  name: "Charmander",
-  height: '2\'',
-  type: 'fire'
-}];
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 3,
+    type: "grass,poison",
+  },
+  {
+    name: "Squirtle",
+    height: 1,
+    type: "water",
+  },
+  {
+    name: "Charmander",
+    height: 2,
+    type: "fire",
+  },
+];
 
 //loop that list all pokemon with the height and name
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write("Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + ".");
+  document.write(
+    "Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + "."
+  );
+
+  //conditional if a pokemon has a height above 2 it is big
+  if (pokemonList[i].height > 2) {
+    document.write(pokemonList[i].height + "-Wow that is big");
+  }
+
+  document.write("<br>");
 }
-//conditional if a pokemon has a height above 2 it is big
-if (pokemonList.height < 2);
-document.write(pokemonList[i].height + 'Wow that is big');
