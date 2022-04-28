5a8d3359acdf9d212afefe59eb927bcd3c75c030
Olvar Abadeen <oabadeen@gmail.com>
2022-04-28 00:45:26 -0700

    corrected IIFE and Added "add" function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepository = (function() {
   let pokemonList = [{
       name: "Bulbasaur",
-      height: 3,
+      height: 2,
       type: "grass,poison",
     },
     {
@@ -15,29 +15,39 @@ let pokemonRepository = (function() {
       height: 2,
       type: "fire",
     },
+    {
+      name: "Pikachu",
+      height: 1,
+      type: "electric"
+    },
+    {
+      name: "Nidoqueen",
+      height: 4,
+      type: "poison, ground"
+    }
   ];
 
   function getAll(){
     return pokemonList;
   }
 
+  function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
   return {
+    add: add,
     getAll: getAll
   }
+
 })();
 pokemonRepository.getAll().forEach(function (pokemon) {
-  console.log(pokemon.name)
+  document.write("name: " + pokemon.name + " height: " + pokemon.height + " type: " + pokemon.type);
+
+  //conditional if a pokemon has a height above 2 it is big
+  if (pokemon.height > 3) {
+    document.write(" - Wow that is big");
+  }
+
+  document.write("<br>");
 });
-
-//forEach Loop
-pokemonList.forEach(function(pokemon) {
-  document.write("name: " + pokemon.name + " " + "height: " + pokemon.height + " " + "type:" + pokemon.type);
-});
-
-//conditional if a pokemon has a height above 2 it is big
-if (pokemonList[i].height > 2) {
-  document.write(pokemonList[i].height + "-Wow that is big");
-}
-
-document.write("<br>");
-}
